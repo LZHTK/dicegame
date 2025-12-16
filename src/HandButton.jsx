7c974f42900270358e11fa916ed6f5ea5b6625a1
LZHTK <gusdnjsrl2@gmail.com>
2025-12-16 15:15:06 +0900

    [feat] 인라인 스타일 적용

--- src/HandButton.jsx
+++ src/HandButton.jsx
@@ -1,9 +1,24 @@
 import HandIcon from "./HandIcon";
 
+const handButtonStyle = {
+    display: 'inline-flex',
+    width: '120px',
+    height: '120px',
+    padding: '30px',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: '32px',
+    background: 'rgba(255, 255, 255, 0.03)',
+    boxShadow: '0px 4px 20px 0px rgba(0, 0, 0, 0.1)',
+    cursor: 'pointer',
+    border: 'none',
+    outline: 'none',
+};
+
 function HandButton({ value, onClick }) {
 
     return (
-        <button  onClick={onClick}>
+        <button  style={handButtonStyle} onClick={onClick}>
             <HandIcon value={value} />
         </button>
     );
